cecfc2571b426e756eb121ac18cac232df70d709
AdonTheDev <110767750+AdonTheDev@users.noreply.github.com>
2025-04-05 11:56:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 app.use(cors());
